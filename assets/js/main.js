b3feb9815218de131d3979ec5db61a1da32333e6
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-06-24 15:04:41 +0300

    gitignore updated

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+const input = document.querySelector("#input");
+const searchbtn = document.querySelector("#searchbtn");
+const clearbtn = document.querySelector("#clearbtn");
+
+const imageWrapper = document.querySelector(".imageWrapper");
+const form = document.querySelector("#form");
+
+runEventListenners();
+
+function runEventListenners() {
+  form.addEventListener("submit", search);
+  clearbtn.addEventListener("click", clear);
+}
+
+function search(e) {
+  let value = input.value.trim();
+  if (value == "nazz" || value == "taha") {
+    value = "ape";
+  }
+  if (value.length > 0) {
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Client-ID ${CLIENT_ID}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        Array.from(data.results).forEach((image) => {
+          addUrlsToImage(image.urls.small);
+        });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    alert("İlk Önce Bize Ne Aramak İstediğini Yaz ");
+  }
+
+  clear();
+  e.preventDefault();
+}
+
+function addUrlsToImage(url) {
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = url;
+  img.height = img.width = 300;
+  div.appendChild(img);
+  imageWrapper.appendChild(div);
+}
+
+function clear() {
+  imageWrapper.innerHTML = "";
+  input.value = "";
+}
